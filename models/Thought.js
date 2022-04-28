f35500e65f8a3a1e052bbecda4eb46b7d6a33b64
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-04-28 03:06:32 -0500

    added dateFormat util, set up thought schema

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema({});
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -35,7 +35,7 @@ const UserSchema = new Schema(
     }
 );
 
-UserSchema.virtual('firendCount').get(function() {
+UserSchema.virtual('friendCount').get(function() {
     return this.friends.length
 })
 
