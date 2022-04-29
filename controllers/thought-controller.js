9bc0179f8caa5bff2e31659a635705945f73506f
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-04-29 03:43:49 -0500

    new thoughts are added to user's thoughts array

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,4 +1,4 @@
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
 const thoughtController = {
     // get all thoughts
@@ -35,7 +35,20 @@ const thoughtController = {
     // create thought
     createThought({ body }, res) {
         Thought.create(body)
-        .then(thoughtData => res.json(thoughtData))
+        .then(thoughtData => {
+            User.findOneAndUpdate(
+                { username: body.username },
+                { $push: { thoughts: thoughtData._id } },
+                { new: true, runValidators: true }
+            )
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: "No user found with this username." });
+                    return;
+                }
+                res.json(thoughtData)
+            })
+        })
         .catch(err => res.status(400).json(err));
     },
 
